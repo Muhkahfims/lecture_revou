7b8fc11a376eadd38ceb7873d095bac304e2d70f
Muhkahfims <114171924+Muhkahfims@users.noreply.github.com>
2024-05-06 21:58:55 +0800

    week 11

---
+++ Week-11/index.js
@@ -0,0 +1,19 @@
+// console.log(Boolean(100))
+// console.log(Boolean(1))
+// console.log(Boolean(0))
+// console.log(Boolean(-5))
+
+// let x = 0;
+// console.log(x);
+// x=2;
+// console.log(x);
+
+const x = false;
+const y = true;
+console.log(x && y);
+console.log(x || y);
+console.log(!x);
+console.log( !y);
+
+// let nama = 'kahfi'
+// console.log(typeof name)

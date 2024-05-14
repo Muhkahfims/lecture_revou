711294595cf09ccac7ef788c91f4fdc870388ac3
Muhkahfims <114171924+Muhkahfims@users.noreply.github.com>
2024-05-14 21:35:17 +0800

    week 12 - lecture 2

--- Week-12/Lecture 4/index.js
+++ Week-12/Lecture 4/index.js
@@ -42,12 +42,68 @@ const text = document.getElementById('text')
 //appendChild dipakai untuk mengambil API
 //Ambil data dan menampilkan data dari API
 
-const paragraphs = document.getElementsByClassName("paragraph");
-for(const paragraph of paragraphs){
-    paragraph.style.color = "blue";
+// const paragraphs = document.getElementsByClassName("paragraph");
+// for(const paragraph of paragraphs){
+//     paragraph.style.color = "blue";
+// }
+
+
+// let parent = document.querySelector(".parent");
+// let child = parent.querySelector(".child");
+// alert(child.parentElement.className);
+
+// cara baru 1
+const newButton = document.getElementById('newButton');
+
+newButton.addEventListener("click",() => {
+    alert("Hello World");
+});
+
+
+// cara baru 2
+// newButton.addEventListener("click", clickButton);
+// const clickButton = () => {
+//     alert("hello world")
+// }
+// function sum(a,b) {
+//     return a+b;
+// }
+
+const sum = (a,b) => {
+    return a+b
+}
+// alert(sum(1,2));
+
+
+const myName = "Kahfi"
+//Example Template Leteral
+const resultMessage = `Hasil penjumlahan 1 tambah 2 adalah ${sum(1,2)} \n dan nama saya ${myName}`;
+// alert(resultMessage)
+
+// const greet = (name) => {
+//     return `Hello, ${name}`
+// }
+
+// Destructuring
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const [one, two, ,four, , , seven]= numbers;
+
+// alert(`one: ${one}, two: ${two}, four: ${four}, seven: ${seven}`);
+
+//Destructuring Object
+let person = {
+    name : 'kahfi',
+    age: 21,
+    job:'student',
+    gender:'L'
 }
 
+const {name,gender} = person
+alert(`name: ${name}, gender: ${gender}`)
 
-let parent = document.querySelector(".parent");
-let child = parent.querySelector(".child");
-alert(child.parentElement.className);
+//Spread syntax
+const array1 = [1,2,3];
+const array2 = [4,5,6];
+const mergedArray = [...array2, ...array1]
+alert(mergedArray)
+
